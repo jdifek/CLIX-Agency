c36cf3ca3f239911de203e8a6abec0bdc288de2c
jdifek <givotkov11@gmail.com>
2024-12-16 16:06:06 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import TechStack from "@/components/home/TechStack";
 import AboutSection from "@/components/home/AboutSection";
 import ServicesSection from "@/components/home/ServicesSection";
 
+
 export default function Home() {
   return (
     <main>
